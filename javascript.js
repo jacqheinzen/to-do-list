bdd1b94eb74057362ceb1b7f2032edafdf318a10
jacqheinzen <42900956+jacqheinzen@users.noreply.github.com>
2018-11-30 12:43:21 -0500

    Add javascript to create task divs

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 $('.add-item').click(function(){
-    $('.modal').show();
+    $('.modal').show(500);
 });
 
 $('.exit-modal').click(function() {
-    $('.modal').hide();
+    $('.modal').hide(500);
+});
+
+$('.button').click(function() {
+    event.preventDefault();
+    var input = $('.text-input').val();
+    var newTask = $("<div></div>").text(input);
+    $('.list').append(newTask);
+    $('.modal').hide(500);
 });
