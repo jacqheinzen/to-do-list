46b2b19cedaabab3a4a504adacf3513752f7dcb7
jacqheinzen <42900956+jacqheinzen@users.noreply.github.com>
2018-12-04 17:19:03 -0500

    Add sidebar

--- javascript.js
+++ javascript.js
@@ -13,3 +13,6 @@ $('.button').click(function() {
     $('.list').append(newTask);
     $('.modal').hide(500);
 });
+
+//Add sub-task function to sidebar - maybe doesn't appear until you click on a task?
+//add deadline and checkbox to new tasks
