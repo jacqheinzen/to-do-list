a7a9bc29ca45794e2500512fdb101c0857bb55b3
jacqheinzen <42900956+jacqheinzen@users.noreply.github.com>
2018-12-06 21:45:21 -0500

    Add category function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-const category = $('.category').children("option:selected").val();
-console.log(category);
 $('.add-item').click(function(){
     $('.modal').show(500);
 });
@@ -12,11 +10,9 @@ $('.button').click(function() {
     event.preventDefault();
     var input = $('.text-input').val();
     var checkbox = $('<input/>').attr({type: 'checkbox', name: 'chk'}).addClass('chk');
-    var newTask = $("<div></div>").text(input).addClass('task').prepend(checkbox);
+    var category = $('.category').children("option:selected").val();
+    var newTask = $("<div></div>").text(input).addClass('task').addClass(category).prepend(checkbox);
     $('.list').append(newTask);
     $('.modal').hide(500);
 });
-
-
-
 //Add sub-task function to sidebar - maybe doesn't appear until you click on a task?
