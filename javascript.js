5faf91763bf7628767a3f46f024d0941d396f377
jacqheinzen <42900956+jacqheinzen@users.noreply.github.com>
2018-12-09 16:29:02 -0500

    Add remove button

--- javascript.js
+++ javascript.js
@@ -6,17 +6,26 @@ $('.exit-modal').click(function() {
     $('.modal').hide(500);
 });
 
+
 $('.button').click(function() {
     event.preventDefault();
     var input = $('.text-input').val();
     var checkbox = $('<input/>').attr({type: 'checkbox', name: 'chk'}).addClass('chk');
+    var remove = $("<input/>").attr({type: 'button', id: 'remove', value: "Remove"}).addClass('remove');
     var category = $('.category').children("option:selected").val();
-    var newTask = $("<div></div>").text(input).addClass('task').addClass(category).prepend(checkbox);
+    var newTask = $("<div></div>").text(input).addClass('task').addClass(category).prepend(checkbox).append(remove);
     $('.list').append(newTask);
     $('.modal').hide(500);
 });
 
 $('.list').on("click", '.chk', function() {
-        var clicked = $(this).parent();
-        $(clicked).css({"order": "1", "text-decoration": "line-through", "opacity": ".5"});
+    var clicked = $(this).parent();
+    $(clicked).css({"order": "1", "text-decoration": "line-through", "opacity": ".7"});
 });
+
+$('.list').on("click", '.remove', function() {
+    var clicked = $(this).parent();
+    $(clicked).css({"display": "none"});
+});
+
+//add delete/edit functionality
